be3d390c668bff18e4410646dbc18984885aac49
Marcos Antonio Villegas Padilla <marcvill26@gmail.com>
2022-04-06 23:21:55 +0200

    style modify

--- src/components/CardcomicList/CardcomicList.jsx
+++ src/components/CardcomicList/CardcomicList.jsx
@@ -5,23 +5,21 @@ export default function CardcomicList({ ...rest }) {
   return (
     
 <div className="card">
-<div className= "comic-card">
+  <div className= "comic-card">
     <div className= "comic-card__info">
-
-      <div className="comic-card__hero">hero: {hero}</div>
-      <div className="comic-card__name">name: {nameHero}</div>
-      <div className="comic-card__divider"></div>
-      <p className="comic-card__bio">desciption: {description}</p>
-      <button className="comic-card__button">read</button>
+        <div className="comic-card__hero"><span>hero:</span> {hero}</div>
+        <div className="comic-card__name"><span>name:</span> {nameHero}</div>
+        <div className="comic-card__divider"></div>
+        <p className="comic-card__bio"><span>desciption:</span> {description}</p>
+        <button className="comic-card__button">read</button>
     </div>
-   <img src={image} alt="comics" className="comic-card__image"></img>
-   <div className="comic-card__footerInfo">
-   <p>comic: {hero}</p>
-   <p>writer: {writer}</p>
-   <p>year: {year}</p>
-   </div>
-   </div>
-
+        <img src={image} alt="comics" className="comic-card__image"></img>
+    <div className="comic-card__footerInfo">
+      <p>comic: {hero}</p>
+      <p>writer: {writer}</p>
+      <p>year: {year}</p>
+    </div>
+    </div>
 </div>
    
 
