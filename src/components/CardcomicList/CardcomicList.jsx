87c067559ffb57b1d0998eae0890577acbdcd1fc
Marcos Antonio Villegas Padilla <marcvill26@gmail.com>
2022-04-06 20:45:17 +0200

    display-flex + hover cards

--- src/components/CardcomicList/CardcomicList.jsx
+++ src/components/CardcomicList/CardcomicList.jsx
@@ -3,24 +3,53 @@ import React from 'react';
 export default function CardcomicList({ ...rest }) {
   const { hero, nameHero, writer, year, image, description, collection } = rest;
   return (
-    <div className= "comic-card">
-       
-        <img src={image} alt="comics" className="comic-card__image"></img>
-        {/* {cardList.map((hero,nameHero, index, img) => {
-            return (
-                <div key={index}>
-                <img src={img.image} alt="comics" className="comic-img"></img>
-                    <p>{hero.hero}</p>
-                    <p>${nameHero.nameHero}</p>
-            </div>
-            )
-        })} */}
-        <h2>comic:</h2>
-        <h3>hero: {hero}</h3>
-        <p>name: {nameHero}</p>
-        <p>writer: {writer}</p>
-        <p>year: {year}</p>
+    
+<div className="card">
+<div className= "comic-card">
+    <div className= "comic-card__info">
+
+      <div className="comic-card__hero">hero: {hero}</div>
+      <div className="comic-card__name">name: {nameHero}</div>
+      <div className="comic-card__divider"></div>
+      <p className="comic-card__bio">desciption: {description}</p>
+      <button className="comic-card__button">read</button>
     </div>
+   <img src={image} alt="comics" className="comic-card__image"></img>
+   <div className="comic-card__footerInfo">
+   <p>comic: {hero}</p>
+   <p>writer: {writer}</p>
+   <p>year: {year}</p>
+   </div>
+   </div>
+
+</div>
+   
+
+    // <div class="comic-card">
+    //     <div class="comic-card__element">
+    //        <img src={image} alt="comics" className="comic-card__image"></img>
+    //         <h2>comic:</h2>
+    //         <h3 className= "comic-card__hero">hero: {hero}</h3>
+           
+    //         <p className= "comic-card__writer">writer: {writer}</p>
+    //         <p className= "comic-card__year">year: {year}</p>
+    //     </div>
+    //   </div>
+    
   )
 }
 
+/*
+<div className= "comic-card">
+    <div className= "comic-card__info">
+
+      <div className="comic-card__name">hero: {hero}</div>
+      <div className="comic-card__prof"></div>
+      <div className="comic-card__divider"></div>
+      <p className="comic-card__bio">desciption: {description}</p>
+      <button>read</button>
+    </div>
+   <img src={image} alt="comics" className="comic-card__image"></img>
+</div>
+
+*/ 
