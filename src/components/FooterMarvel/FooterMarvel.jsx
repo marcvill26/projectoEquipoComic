d77c83149aefc4f1ea6e3bee7fe32835d201d808
Marcos Antonio Villegas Padilla <marcvill26@gmail.com>
2022-05-02 18:29:28 +0200

    footer creaded

--- src/components/FooterMarvel/FooterMarvel.jsx
+++ src/components/FooterMarvel/FooterMarvel.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
-
+import Marvel from '../../assets/image/Marvel-Roster-Characters-Wallpaper.png'
 export default function FooterMarvel() {
   return (
-    <div>FooterMarvel</div>
+    <div className="footer">
+      <div className="footer__info">
+        <ul>
+          <li className="footer__link">about us</li>
+          <li className="footer__link">help/faqs</li>
+          <li className="footer__link">subscriptions</li>
+          <li className="footer__link">shop help</li>
+        </ul>
+        <ul>
+          <li className="footer__link">videos</li>
+          <li className="footer__link">comics dc</li>
+          <li className="footer__link">comics marvel</li>
+          <li className="footer__link">contact us</li>
+        </ul>
+      </div>
+      
+    </div>
   )
 }
